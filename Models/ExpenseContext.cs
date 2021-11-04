84de81c757905da2e009be24fead685a513233b7
Carmen Bondane <carmenbondane@yahoo.com>
2021-11-04 19:20:11 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bondane_Carmen_Lab5_EB.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+
+    }
+}
